a1d538b94b9ac61370c937479178cf3540e5e91c
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-01-29 18:07:58 -0500

    Validaciones para Registro de Customer

--- back-end/controllers/customer.controller.js
+++ back-end/controllers/customer.controller.js
@@ -1,14 +1,34 @@
 'use strict'
 
 let customer = require('../models/customer.model');
+let bcrypt = require('bcrypt-nodejs');
 
 const customerRegister = async function(req, res) {
     let data = req.body;
+    let customersList = [];
 
-    let reg = await customer.create(data);
-    console.log(reg);
+    customersList = await customer.find({email:data.email});
 
-    res.status(200).send({ message: reg });
+    if (customersList.length == 0) {
+        if (data.password) {
+            bcrypt.hash(data.password, null, null, async function(err, hash) {
+                if (hash) {
+                    data.password = hash;
+                    let reg = await customer.create(data);
+                    res.status(200).send({ data: reg });
+                }
+                else {
+                    res.status(200).send({ message: "Error hashing password", data: undefined });
+                }
+            });
+        }
+        else {
+            res.status(200).send({ message: "Password required", data: undefined });
+        }
+    }
+    else {
+        res.status(200).send({ message: "E-mail not available", data: undefined });
+    }
 }
 
 module.exports = {
