64c19aa42a35197975192c56e6217fb9717e0b69
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-02-04 13:37:43 -0500

    Cambio en gap de Productos y en hover de sus imgs

--- front-end/src/app/services/products.service.ts
+++ front-end/src/app/services/products.service.ts
@@ -16,16 +16,22 @@ export class ProductsService {
         price: 120000
       },
       {
-        description: 'Vestido',
+        description: 'Camiseta',
         category: "Colorido",
         imageURL: 'https://img.freepik.com/free-psd/t-shirt-colorful-tie-dye-print-women-apparel_53876-113686.jpg?w=2000',
-        price: 95000
+        price: 45000
       },
       {
         description: 'Camiseta',
         category: 'Minimalista',
         imageURL: 'https://i.pinimg.com/originals/a8/7c/7a/a87c7ab1dd72fb450ecec573616d64be.jpg',
         price: 50000
+      },
+      {
+        description: 'Jogger',
+        category: 'Colorido',
+        imageURL: 'https://i.etsystatic.com/26424606/r/il/4c5f0c/3895668949/il_fullxfull.3895668949_q679.jpg',
+        price: 140000
       }
     ]
   }
