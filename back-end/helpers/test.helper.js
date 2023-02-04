81f62babcf00a866c4c1438a0afd3f83a419758e
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-02-04 15:28:52 -0500

    Archivos de back-end antiguos eliminados

--- back-end/helpers/test.helper.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-exports.sendMessage = function(text, req, res) {
-    res.status(200).send({msg: text});
-}
--- back-end/routes/test.route.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let express = require('express');
-let testController = require("../controllers/test.controller");
-
-let api = express.Router();
-api.post('/testFunction', testController.testFunction);
-
-module.exports = api;
